9c48b7afb8240cc0be355886e7096576378a4748
Jesse Gibson <overlord@Marvin.T-mobile.com>
2015-09-22 15:52:56 -0600

    Added view method to draw grid lines on the canvas.

--- game-controller.js
+++ game-controller.js
@@ -1,1 +1,4 @@
 // CONTROLLER
+var game = {
+
+};
--- view.js
+++ view.js
@@ -7,27 +7,60 @@
   toggle cell
 */
 
-var view;
+var view,
+  canvas = document.querySelector('canvas'),
+  ctx = canvas.getContext('2d');
 
 (function () {
   'use strict';
 
-  var canvas = document.getElementsByTagName('canvas')[0],
-    context = canvas.getContext('2d'),
-    cellWidth = 20,
-    cellHeight = cellWidth;
-
   view = {
-    render: function (cellGroup) {
-      cellGroup.each(view.sendToCanvas);
+    port: {
+      x: 0,
+      y: 0
     },
-    sendToCanvas: function (cell) {
-      view.drawCell(cell);
-    },
-    drawCell(cell) {
 
-      context.rect();
+    scale: 1,
+
+    cell: {
+      width: 20
+    },
+
+    borderWidth: .08,
+
+    drawGridLines: function() {
+      var row,
+          col,
+          cellHeight = view.cell.height + view.borderWidth,
+          cellWidth = view.cell.width + view.borderWidth;
+
+      ctx.beginPath();
+      for (row = 0; row < canvas.height / cellHeight; row++) {
+        if (row === 0) {
+          ctx.moveTo(col * view.cell.height, 0);
+          ctx.lineTo(col * view.cell.height, canvas.height);
+        } else {
+          ctx.moveTo(0, row * cellHeight);
+          ctx.lineTo(canvas.width, row * cellHeight);
+        }
+      }
+      for (col = 1; col < canvas.width / cellWidth; col++) {
+        if (col === 1) {
+          ctx.moveTo(col * view.cell.width, 0);
+          ctx.lineTo(col * view.cell.width, canvas.height);
+        } else {
+          ctx.moveTo(col * cellWidth, 0);
+          ctx.lineTo(col * cellWidth, canvas.height);
+        }
+      }
+      ctx.lineWidth = view.borderWidth;
+      ctx.strokeStyle = "#303438"
+      ctx.stroke();
+      ctx.closePath;
     }
   };
 
 }());
+
+view.cell.height = view.cell.width;
+view.drawGridLines();
